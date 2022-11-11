746a3160be47db6018163cb90b81e61676ab724f
Javivy <84439052+Javivy@users.noreply.github.com>
2022-11-11 17:18:07 -0400

    Add navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-unused-vars */
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => (
     <div className='desktop-navbar'>
-      <h1 className='navbar-title'>Bookstore</h1>
-      <Link to='/' className="link-item">Books</Link> |
-      <Link to='/categories'> Categories</Link>
+      <div className='navbar-header'>
+        <h1 className='navbar-title'>Bookstore CMS</h1>
+        <Link to='/' className="link-item">Books</Link>
+        <Link to='/categories' className='link-item'> Categories</Link>
+      </div>
+      <div>
+        <FontAwesomeIcon className='user-icon' icon={faUser}/>
+      </div>
     </div>
 );
 
