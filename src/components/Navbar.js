87bcc8b0e2264f344a466d65f19ebf6b399eb247
Javivy <84439052+Javivy@users.noreply.github.com>
2022-11-11 23:49:38 -0400

    Style active links in the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,26 @@
 /* eslint-disable no-unused-vars */
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-const Navbar = () => (
+const Navbar = () => {
+  const activeStyle = {
+    color: '#000',
+    fontWeight: 'bold',
+  };
+
+  return (
     <div className='desktop-navbar'>
       <div className='navbar-header'>
         <h1 className='navbar-title'>Bookstore CMS</h1>
-        <Link to='/' className="link-item">Books</Link>
-        <Link to='/categories' className='link-item'> Categories</Link>
+        <NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} to='/' className="link-item">Books</NavLink>
+        <NavLink style={({ isActive }) => (isActive ? activeStyle : undefined)} to='/categories' className='link-item'> Categories</NavLink>
       </div>
       <div>
         <FontAwesomeIcon className='user-icon' icon={faUser}/>
       </div>
     </div>
-);
+  );
+};
 
 export default Navbar;
