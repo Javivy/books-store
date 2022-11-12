07fb197eba3538439714867f0c5bedfcdee21a89
Javivy <84439052+Javivy@users.noreply.github.com>
2022-11-11 23:03:35 -0400

    Style book container

--- src/components/Book.js
+++ src/components/Book.js
@@ -19,21 +19,21 @@ const Book = () => {
           <p className='book-title'>{book.title}</p>
           <p className='book-author'>{book.author}</p>
           <div className='book-links'>
-            <a className='book-link-item' href='#'>Comment</a>
+            <a className='book-link-item'>Comment</a>
             <a onClick={() => {
               dispatch(removeBook(book.item_id));
-            }} className='book-link-item' href='#'>Remove</a>
-            <a className='book-link-item' href='#'>Edit</a>
+            }} className='book-link-item'>Remove</a>
+            <a className='book-link-item'>Edit</a>
           </div>
         </div>
         <div className='book-progress-info'>
           <div className='progress-made'>
             <svg className='circle-progress'>
-              <circle cx='70' cy='70' r='70'></circle>
-              <circle cx='70' cy='70' r='70'></circle>
+              <circle cx='50' cy='50' r='50'></circle>
+              <circle cx='50' cy='50' r='50'></circle>
             </svg>
             <div className='progress-info'>
-              <span className='progress'>64%</span>
+              <span className='progress-number'>64%</span>
               <span className='progress-completed'>Completed</span>
             </div>
           </div>
@@ -44,7 +44,7 @@ const Book = () => {
           <div className='progress-chapter'>
             <p className='current-chapter'>Current Chapter</p>
             <p className='chapter-number'>Chapter 17</p>
-            <button className='Update Progress'>Update Progress</button>
+            <button>Update Progress</button>
           </div>
 
         </div>
